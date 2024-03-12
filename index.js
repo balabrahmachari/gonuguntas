c4f705b2c7c653080b3998d19de41360e998df26
balabrahmachari <153415405+balabrahmachari@users.noreply.github.com>
2024-03-12 21:31:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+let ab = document.getElementsByClassName("y")[0];
+let ggf = () => {
+    ab.style.display = "block"
+    // if(ab.style.display == "block"){
+    //     ab.style.display = "none";
+    // }
+    // else{
+    //     ab.style.display = "block";
+    // }
+}
+
+let a=()=>{
+    let aa=document.getElementById("z")
+    aa.style.display="block"
+}
+let b=()=>{
+    let aa=document.getElementById("zbb")
+    aa.style.display="block"
+}
+let c=()=>{
+    let aa=document.getElementById("zcc")
+    aa.style.display="block"
+}
